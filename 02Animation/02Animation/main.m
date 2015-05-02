969a0f84fb671ab7ad527b418aff942905a49f97
Milos Jovanovic <milos.jovanovic@cbc.ca>
2015-05-02 17:17:55 -0400

    Added presentation file and second example project

---
+++ 02Animation/02Animation/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  02Animation
+//
+//  Created by Milos Jovanovic on 2015-05-02.
+//  Copyright (c) 2015 ca.cbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
