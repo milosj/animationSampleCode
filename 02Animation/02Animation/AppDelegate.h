d78ea07bdd4d2ea7ef8e76df87112123b3e99715
Milos Jovanovic <milos.jovanovic@cbc.ca>
2015-05-02 17:17:55 -0400

    Added presentation file and second example project
    Former-commit-id: 969a0f84fb671ab7ad527b418aff942905a49f97

---
+++ 02Animation/02Animation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  02Animation
+//
+//  Created by Milos Jovanovic on 2015-05-02.
+//  Copyright (c) 2015 ca.cbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 02Animation/02Animation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  02Animation
+//
+//  Created by Milos Jovanovic on 2015-05-02.
+//  Copyright (c) 2015 ca.cbc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
