4bf54cb684a11a0b0bf77bed4d65aa4c852e1a0c
Milos Jovanovic <milos.jovanovic@cbc.ca>
2015-05-29 16:07:01 -0400

    UI example 03
    Former-commit-id: ecaaae91e13ead3d7053f5703d81ef2ff33580a9

---
+++ 03UI/03UI/TableViewController.m
@@ -0,0 +1,224 @@
+//
+//  TableViewController.m
+//  02UI
+//
+//  Created by Milos Jovanovic on 2015-05-28.
+//  Copyright (c) 2015 ca.cbc. All rights reserved.
+//
+
+#import "TableViewController.h"
+
+@interface TableViewController ()
+@property (strong, nonatomic) UIImageView* imageView;
+@property (strong, nonatomic) UIView* maskView;
+@property (strong, nonatomic) CIFilter* bump;
+@property (strong, nonatomic) CIContext *context;
+@property (strong, nonatomic) NSOperationQueue* workQueue;
+@property (assign, nonatomic) CGPoint lastTouchPoint;
+
+@property (strong, nonatomic) NSArray* data;
+
+@end
+
+@implementation TableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+    
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+    
+    
+    //set up a filter instance and context
+    //we want to do only once for perfomance reasons
+    self.bump = [CIFilter filterWithName:@"CIHoleDistortion"];
+    [self.bump setValue:[CIVector vectorWithCGPoint:CGPointMake(187, 282)] forKey:kCIInputCenterKey];
+    [self.bump setValue:@100 forKey:kCIInputRadiusKey];
+    self.context = [CIContext contextWithOptions:nil];
+
+    //set up a background queue to do CI work
+    self.workQueue = [NSOperationQueue new];
+    
+    self.maskView = [UIView new];
+    self.maskView.backgroundColor = [UIColor whiteColor];
+    self.maskView.hidden = YES;
+    [self.view addSubview:self.maskView];
+    
+    self.imageView = [UIImageView new];
+    self.imageView.hidden = YES;
+    [self.view addSubview:self.imageView];
+    
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        NSURL* url = [NSURL URLWithString:@"http://cbc.ca/json/cmlink/1.3084190"];
+        self.data = [[NSJSONSerialization JSONObjectWithData:[NSData dataWithContentsOfURL:url] options:0 error:nil] objectForKey:@"galleryImages"];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [self.tableView reloadData];
+        });
+    });
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    self.maskView.frame = self.view.bounds;
+    self.imageView.frame = self.view.bounds;
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewWillTransitionToSize:(CGSize)size withTransitionCoordinator:(id<UIViewControllerTransitionCoordinator>)coordinator {
+    [super viewWillTransitionToSize:size withTransitionCoordinator:coordinator];
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+
+    // Return the number of rows in the section.
+    return self.data.count;
+}
+
+- (UITableViewCell*)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell* cell = [tableView dequeueReusableCellWithIdentifier:@"imageCell" forIndexPath:indexPath];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+        NSArray* sizes = [self.data[indexPath.row] objectForKey:@"imageFiles"];
+        NSDictionary* bestSize = [sizes lastObject];
+        NSURL* url = [NSURL URLWithString:bestSize[@"file"]];
+        NSData* imgData = [NSData dataWithContentsOfURL:url];
+        UIImage* img = [UIImage imageWithData:imgData];
+        if ([tableView.visibleCells containsObject:cell]) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                UIImageView* imageView = (UIImageView*)[cell viewWithTag:1];
+                imageView.image = img;
+            });
+        }
+    });
+    
+    
+    return cell;
+}
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    self.maskView.hidden = YES;
+    self.imageView.hidden = YES;
+
+    //user began dragging - grab the screen and save it
+    CGPoint previousOffset = scrollView.contentOffset;
+    scrollView.contentOffset = CGPointZero; //we want the image of the top of the scrollview content
+    UIGraphicsBeginImageContextWithOptions(self.view.bounds.size, self.view.opaque, 0.0);
+    [self.view.layer renderInContext:UIGraphicsGetCurrentContext()];
+    
+    UIImage * img = UIGraphicsGetImageFromCurrentImageContext();
+    
+    UIGraphicsEndImageContext();
+    CGImageRef cgImage = img.CGImage;
+    CIImage* cimg = [[CIImage alloc] initWithCGImage:cgImage];
+    [self.bump setValue:cimg forKey:kCIInputImageKey];
+    self.imageView.image = img;
+    scrollView.contentOffset = previousOffset; //return the scrollview to where it was
+
+
+
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    //hide our special views and go back to the tableview
+    self.maskView.hidden = YES;
+    self.imageView.hidden = YES;
+    
+}
+
+- (void)scrollViewDidEndScrollingAnimation:(UIScrollView *)scrollView {
+
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    CGFloat dY = scrollView.contentOffset.y;
+    if (dY < 0) { //if pulling down
+        //show the mask (which hides the table view and provides white background)
+        //show the imageView that will contain our filtered image
+        self.maskView.hidden = NO;
+        self.imageView.hidden = NO;
+        if (self.workQueue.operationCount < 10) { //we'll keep a number of operations to a minimum to reduce lag
+            //find where the user's finger is
+            CGPoint touchPoint = [scrollView.panGestureRecognizer locationInView:scrollView];
+            if (powf(powf(self.lastTouchPoint.x-touchPoint.x,2.0f) + powf(self.lastTouchPoint.y-touchPoint.y,2.0f),0.5f) > 5.0f) {
+                [self.workQueue addOperationWithBlock:^{
+                    //center the distortion above the finger
+                    [self.bump setValue:[CIVector vectorWithCGPoint:CGPointMake(touchPoint.x*self.traitCollection.displayScale, self.view.frame.size.height*self.traitCollection.displayScale)] forKey:kCIInputCenterKey];
+                    //increase the radius of the distortion the more the user pulls the scroll view
+                    [self.bump setValue:[NSNumber numberWithFloat:-MIN(150, dY)] forKey:kCIInputRadiusKey];
+                    //get the filtered image
+                    CIImage *result = [self.bump valueForKey:kCIOutputImageKey];
+                    //transform it into something we can use
+                    CGImageRef cgImage = [self.context createCGImage:result fromRect:result.extent];
+                    UIImage* filteredImage = [UIImage imageWithCGImage:cgImage];
+                    CGImageRelease(cgImage);
+                    dispatch_async(dispatch_get_main_queue(), ^{
+                        //update the imageView
+                        self.imageView.image = filteredImage;
+                    });
+                }];
+            }
+        }
+    }
+}
+
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
---
+++ 03UI/03UI/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  03UI
+//
+//  Created by Milos Jovanovic on 2015-05-29.
+//  Copyright (c) 2015 ca.cbc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
